74a89659889c7d5d8ac026e67686d77f35f0a502
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2021-07-14 21:04:22 -0400

    registration route finished

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const passport = require("passport");
 const db = require("../model/db");
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  let data = req.body;
+
+  db.addUser(data)
+    .then((user) => {
+      res.json({ message: "Registration successful, " + user.username });
+    })
+    .catch((err) => {
+      if (err instanceof Error) {
+        console.log(err);
+        res.json({ message: "An error ocurred, try again, please." });
+      } else {
+        res.json({ messages: err, message: "Some of data is incorrect." });
+      }
+    });
+});
 
 module.exports = router;
