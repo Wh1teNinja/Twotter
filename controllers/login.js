69de50398cfdeeb09a31829d49b37616a5e7020f
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2021-07-14 21:03:52 -0400

    login route finished with passport authenticate

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
-router.post("/", (req, res, next) => {});
+router.post("/", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (user) {
+      req.logIn(user, (err) => {
+        if (err)  {
+          console.log(err);
+          res.json({ message: "An error ocurred, try again, please." });
+        }
+        res.json({ message: "Welcome, " + user.username });
+      });
+    } else {
+      if (err) {
+        console.log(err);
+        res.json({ message: "An error ocurred, try again, please." });
+      } else {
+        res.json({ messages: info, message: "Some of data is incorrect." });
+      }
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
