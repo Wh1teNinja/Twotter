55a10b53b8cd48eee26424b979d3367b00a4b43a
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2021-07-14 21:20:24 -0400

    finished chat system

--- controllers/chat-socket.js
+++ controllers/chat-socket.js
@@ -5,4 +5,18 @@ module.exports.initialize = async (socket) => {
   var userId = socket.request.session.passport.user;
 
   if (!userId) return;
+
+  db.getAllChatsForUser(userId).then(chats => {
+    for (let i = 0; i < chats.length; i++) {
+      socket.join(chat._id);
+    }
+  }).catch((err) => {socket.emit("server-message", err)});
+  
+  socket.on("send-message", (chat_id, messageText) => {
+    db.chatAddMessage(chat_id, userId, messageText).then(() =>{
+      io.to(chat_id).emit("message", chat.messages[chat.messages.length - 1]);
+    }).catch(err => {
+      socket.emit("server-message", err);
+    });
+  });
 };
--- controllers/chat.js
+++ controllers/chat.js
@@ -3,12 +3,69 @@ const router = express.Router();
 const db = require("../model/db");
 
 // provides list of user's chats
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  db.getAllChatsForUser(req.user._id)
+    .then((chats) => {
+      res.json(chats);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err instanceof Error) {
+        res.json({ message: "An error ocurred, try again, please." });
+      } else {
+        res.json({ message: "No chats have been created yet." });
+      }
+    });
+});
 
 // provides chat history with a certain user
-router.get("/:user_id", (req, res) => {});
+router.get("/:user_id", (req, res) => {
+  db.getAllChatsForUser(req.user._id)
+    .then((chats) => {
+      res.json(
+        chats.find((chat) => {
+          return chat.users.contains(req.params.user_id);
+        })
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err instanceof Error) {
+        res.json({ message: "An error ocurred, try again, please." });
+      } else {
+        res.json({ message: "Chat with this user haven't been created yet." });
+      }
+    });
+});
 
 // http post way to send message to a user
-router.post("/:user_id", async (req, res) => {});
+router.post("/:user_id", async (req, res) => {
+  let chat;
+
+  await db
+    .getAllChatsForUser(req.user._id)
+    .then((chats) => {
+      chat = chats.find((chat) => {
+        return chat.users.contains(req.params.user_id);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err instanceof Error) {
+        res.json({ message: "An error ocurred, try again, please." });
+      } else {
+        db.addChat([req.params.user_id, req.user._id]).then((newChat) => {
+          chat = newChat;
+        });
+      }
+    });
+
+  db.chatAddMessage(chat._id, req.user._id, req.body.message).then((chat) => {
+    res.json(chat);
+  }).catch(err => {
+    console.log(err);
+    res.json({ message: "An error ocurred, try again, please." });
+  });
+});
 
 module.exports = router;
